a0052af26156d6bbbee44ae391743eba2c0e7afd
sashavazhov <sashavazhov@gmail.com>
2021-03-27 11:48:07 +0200

    UPD urls

--- moduleProject/urls.py
+++ moduleProject/urls.py
@@ -5,6 +5,6 @@ from django.urls import path, include
 from moduleProject import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('myapp.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  path('admin/', admin.site.urls),
+                  path('', include('myapp.urls')),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
